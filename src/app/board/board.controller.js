72c90ca90de811ebc9a1dea1f08d1924dac85489
sebastien DESCAMPS <sdescamps.ext@orange.com>
2015-02-27 18:27:01 +0100

    feat: add cards to show

--- src/app/board/board.controller.js
+++ src/app/board/board.controller.js
@@ -3,6 +3,31 @@
 angular.module('choupouxMemo')
   .controller('BoardCtrl', function ($scope) {
 
+      $scope.cardsPictureNameAnimal = [
+        'chat.jpg',
+        'cheval.jpg',
+        'chien.jpg',
+        'cochon.jpg',
+        'kangourou.jpg',
+        'lapin.jpg',
+        'lion.jpg',
+        'mouton.jpg',
+        'ours.jpg',
+        'panda.jpg',
+        'pingoo.jpg',
+        'souris.jpg',
+        'vache.jpg',
+        'zebre.jpg'
+      ];
+
+    $scope.cardsPictureNameMadeHome = [
+        'balle_tennis.png',
+        'batte.png',
+        'cochon.png',
+        'triangles.png',
+        'lait.png'
+    ];
+
   });
 
 

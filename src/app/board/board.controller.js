bc7fdebc6f449f40b37bd39610eb6847a740687a
sdescamps <sebastien.descamps@gmail.com>
2015-03-01 22:07:11 +0100

    feat: add flip feature to card

--- src/app/board/board.controller.js
+++ src/app/board/board.controller.js
@@ -3,7 +3,7 @@
 angular.module('choupouxMemo')
   .controller('BoardCtrl', function ($scope) {
 
-      $scope.cardsPictureNameAnimal = [
+      var cardsPictureNameAnimal = [
         'chat.jpg',
         'cheval.jpg',
         'chien.jpg',
@@ -20,13 +20,48 @@ angular.module('choupouxMemo')
         'zebre.jpg'
       ];
 
-    $scope.cardsPictureNameMadeHome = [
+    var cardsPictureNameMadeHome = [
         'balle_tennis.png',
         'batte.png',
         'cochon.png',
         'triangles.png',
         'lait.png'
     ];
+
+    initCards(cardsPictureNameAnimal, 6);
+
+    $scope.makeFlip = makeFlip;
+
+    function initCards(cardsPicture, nbColonne) {
+      $scope.cardsPictureName = splitCardsPictureTabTab(cardsPicture, nbColonne);
+      $scope.activated = [];
+      cardsPicture.forEach(function(element) {
+        $scope.activated[element] = false;
+      });
+
+    }
+
+    function splitCardsPictureTabTab(cardsPicture, nbColonne) {
+      var tabFinal = [];
+      var row = [];
+      cardsPicture.forEach(function(element, index) {
+        if (row.length > 0 && index % nbColonne == 0) {
+            tabFinal.push(row);
+            row = [];
+        }
+        row.push(element);
+      });
+
+      if (row.length < nbColonne) {
+        tabFinal.push(row);
+      }
+
+      return tabFinal;
+    }
+
+    function makeFlip(key) {
+      $scope.activated[key] = ! $scope.activated[key];
+    }
 
   });
 
